8e15613040a84124ec834da2b0360c4edaa384fe
Süleyman Emre Erdem <121464020+SuleymanEmreErdem@users.noreply.github.com>
2023-02-03 15:03:20 +0300

    Create ginosrtS.py

---
+++ ginosrtS.py
@@ -0,0 +1,17 @@
+S = list(input())
+upper, lower, odd, even = [], [], [], []
+for item in S:
+    if item.isupper():
+        upper.append(item)
+    elif item.islower():
+        lower.append(item)
+    elif item.isdigit():
+        if int(item)%2 == 0:
+            even.append(item)
+        else:
+            odd.append(item)
+upper.sort()
+lower.sort()
+odd.sort()
+even.sort()
+print("".join(lower)+"".join(upper)+"".join(odd)+"".join(even))
